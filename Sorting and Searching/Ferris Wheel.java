ff116a72fec339c285dbfffeb7f048a1ece167cb
pm2am <hsrathore.code@gmail.com>
2023-09-15 19:47:00 +0530

    Ferris Wheel - Greedy implementation

---
+++ Sorting and Searching/Ferris Wheel.java
@@ -0,0 +1,84 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.util.*;
+ 
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		PrintWriter pw = new PrintWriter(System.out);
+		FastScanner sc = new FastScanner();
+		
+		int n,x;
+		n = sc.ni();
+		x = sc.ni();
+		Long[] a = new Long[n];
+		
+		for(int i=0;i<n;i++) {
+			a[i] = sc.nl();
+		}
+		Arrays.sort(a);
+		int ans = 0;
+		int lo = 0, hi = n-1;
+		while(lo<=hi) {
+			if(a[hi] + a[lo] <= x) {
+				ans++;
+				hi--;
+				lo++;
+			} else {
+				ans++;
+				hi--;
+			}
+		}
+		
+		
+		pw.println(ans);
+		
+		
+		
+		pw.close();
+	} 
+	
+
+	   static class FastScanner {
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			StringTokenizer st=new StringTokenizer("");
+			
+			public String ne() {
+				while (!st.hasMoreElements())
+					try {
+						st=new StringTokenizer(br.readLine());
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				return st.nextToken();
+			}
+			public String ne(int k) {
+				try {
+					return br.readLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				return "Not able to read";
+			}
+			
+			int ni() {
+				return Integer.parseInt(ne());
+			}
+			
+			long nl() {
+				return Long.parseLong(ne());
+			}
+			
+			double nd() {
+				return Double.parseDouble(ne());
+			}
+			
+		    
+		}
+ 
+}
