43d7677088978ab34340d104f437cc10639339e3
pm2am <hsrathore.code@gmail.com>
2023-09-18 20:54:47 +0530

    TLE on Collecting Numbers 2 - Using naive approach

---
+++ Sorting and Searching/Collecting Numbers 2.java
@@ -0,0 +1,211 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.util.*;
+ 
+public class Main {
+	
+	
+	public static void main(String[] args) throws IOException {
+		PrintWriter pw = new PrintWriter(System.out);
+		FastScanner sc = new FastScanner();
+//		Reader sc = new Reader();
+		
+		int n = sc.ni();
+		int m = sc.ni();
+		int[] num = new int[n];
+		int[] a = new int[n+1];
+		for(int i=0;i<n;i++) {
+			num[i] = sc.ni();
+			a[num[i]] = i;
+		}
+		while(m-->0) {
+			int x = sc.ni()-1;
+			int y = sc.ni()-1;
+			int x1 = num[x];
+			int x2 = num[y];
+			num[x] = x2;
+			num[y] = x1;
+			a[x1] = y;
+			a[x2] = x;
+
+			int last = n+1;
+			int ans = 0;
+			for(int i=1;i<=n;i++) {
+				if(last>a[i]) {
+					ans++;
+				}
+				last = a[i];
+			}
+			pw.println(ans);
+		}
+		
+
+		
+		pw.close();
+	} 
+	
+	static class Reader {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+  
+        public Reader()
+        {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+  
+        public Reader(String file_name) throws IOException
+        {
+            din = new DataInputStream(
+                new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+  
+        public String readLine() throws IOException
+        {
+            byte[] buf = new byte[64]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1) {
+                if (c == '\n') {
+                    if (cnt != 0) {
+                        break;
+                    }
+                    else {
+                        continue;
+                    }
+                }
+                buf[cnt++] = (byte)c;
+            }
+            return new String(buf, 0, cnt);
+        }
+  
+        public int nextInt() throws IOException
+        {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ') {
+                c = read();
+            }
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+  
+            if (neg)
+                return -ret;
+            return ret;
+        }
+  
+        public long nextLong() throws IOException
+        {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+  
+        public double nextDouble() throws IOException
+        {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+  
+            do {
+                ret = ret * 10 + c - '0';
+            } while ((c = read()) >= '0' && c <= '9');
+  
+            if (c == '.') {
+                while ((c = read()) >= '0' && c <= '9') {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+  
+            if (neg)
+                return -ret;
+            return ret;
+        }
+  
+        private void fillBuffer() throws IOException
+        {
+            bytesRead = din.read(buffer, bufferPointer = 0,
+                                 BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+  
+        private byte read() throws IOException
+        {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+  
+        public void close() throws IOException
+        {
+            if (din == null)
+                return;
+            din.close();
+        }
+    }
+	   static class FastScanner {
+			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			StringTokenizer st=new StringTokenizer("");
+			
+			public String ne() {
+				while (!st.hasMoreElements())
+					try {
+						st=new StringTokenizer(br.readLine());
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				return st.nextToken();
+			}
+			public String ne(int k) {
+				try {
+					return br.readLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				return "Not able to read";
+			}
+			
+			int ni() {
+				return Integer.parseInt(ne());
+			}
+			
+			long nl() {
+				return Long.parseLong(ne());
+			}
+			
+			double nd() {
+				return Double.parseDouble(ne());
+			}
+			
+		    
+		}
+ 
+}
